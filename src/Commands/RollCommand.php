b636641f9108da10b870d8682309257f9107191d
Kris <kris@meridian-ds.com>
2017-02-16 21:10:03 -0600

    initial support of roll: <number> in the same plugin that supports dice rolling

--- src/Commands/RollCommand.php
+++ src/Commands/RollCommand.php
@@ -9,7 +9,7 @@ use Drakythe\Ember\Annotation\Command;
 /**
  * @Command(
  *   pluginId = "roll",
- *   expression = "/^roll: (\d+d\d+[\+*\d]*)/mi",
+ *   expression = "/^roll: (\d+d\d+[\+*\d]*)|(^roll: (\d+))/mi",
  *   help = "roll: <dice and modifier>: Returns results of <dice>. Example 'roll: 2d4+6'"
  * )
  */
@@ -20,9 +20,16 @@ class RollCommand extends CommandBase {
    */
   public function execute(Message $message) {
     foreach ($this->matches[1] as $match) {
+      if ($match[1] == -1) {
+        continue;
+      }
       $match = $match[0];
       $calc = new Calc($match);
       $message->reply("Rolling {$calc->infix()} Result is: {$calc()}");
+    }
+    foreach (array_filter($this->matches[3]) as $match) {
+      $var = rand(1, $match[0]);
+      $message->reply("Result: $var");
     }
   }
 
